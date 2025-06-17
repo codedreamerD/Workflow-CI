24ce9f2d988614e00ea8c6d36c7114259cfccc02
Fadhilah Nurrahmayanti <adnindyakhadijah@gmail.com>
2025-06-17 07:17:07 +0700

    Workflow-CI

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,16 +2,16 @@ import pandas as pd
 import mlflow
 import mlflow.sklearn
 import joblib
+import os
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import os
 
 # Tracking ke MLflow lokal
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("Student_performance_CI")
 
 # Load dataset
-data = pd.read_csv("students_performance_preprocessed.csv")
+data = pd.read_csv("dataset_preprocessed/students_performance_preprocessed.csv")
 
 # Split fitur dan target
 X = data.drop(['math score', 'reading score', 'writing score', 'average_score', 'performance_level'], axis=1)
@@ -23,15 +23,15 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratif
 # Ambil contoh input untuk log model (harus DataFrame)
 input_example = X_train.iloc[0:5]
 
-# Run MLflow autolog
+# Jalankan autolog
 mlflow.autolog()
 
+# Jalankan training dan simpan model
 with mlflow.start_run():
-    # Model Random Forest (parameter tetap, tidak tuning)
     model = RandomForestClassifier(n_estimators=500, max_depth=20, random_state=42)
     model.fit(X_train, y_train)
 
-    # Simpan model ke file (untuk upload ke GitHub)
+    # Simpan model secara manual untuk diupload
     output_path = "models/best_model.pkl"
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     joblib.dump(model, output_path)
